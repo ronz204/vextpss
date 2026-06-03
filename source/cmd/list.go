3437c0bd35f347d78781572aeffe01303fe82eb1
roney ruiz <roneyruizrojas@gmail.com>
2026-06-02 22:44:04 -0600

    feat(cmd): complete list saved accoutns

---
+++ source/cmd/list.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"fmt"
+
+	"vextpss/source/pkg/database"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all stored secrets",
+	Long:  "Displays a table of all stored secret names and types. Does not require the master password.",
+	Args:  cobra.NoArgs,
+	RunE:  runList,
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	dbPath, err := database.DBPath()
+	if err != nil {
+		return err
+	}
+	db, err := database.Open(dbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	records, err := database.ListAll(db)
+	if err != nil {
+		return err
+	}
+
+	if len(records) == 0 {
+		fmt.Println("No secrets stored. Use `vext add <name>` to add one.")
+		return nil
+	}
+
+	fmt.Printf("%-30s  %-12s  %s\n", "NAME", "TYPE", "CREATED")
+	fmt.Printf("%-30s  %-12s  %s\n", "------------------------------", "------------", "-------------------")
+	for _, r := range records {
+		fmt.Printf("%-30s  %-12s  %s\n", r.Name, r.Type, r.CreatedAt.Format("2006-01-02 15:04:05"))
+	}
+
+	return nil
+}
