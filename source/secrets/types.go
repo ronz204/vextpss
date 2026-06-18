7ed54fcf7755aa9b8cd4700cd8a52b89cf9d7da6
roney ruiz <roneyruizrojas@gmail.com>
2026-06-17 22:08:57 -0600

    feat(core): add is know type validator for secret types

--- source/secrets/types.go
+++ source/secrets/types.go
@@ -4,3 +4,13 @@ const (
 	TypeAccount = "account"
 	TypeFinance = "finance"
 )
+
+// IsKnownType reports whether t is a registered secret type.
+// Add new constants here as new types are introduced.
+func IsKnownType(t string) bool {
+	switch t {
+	case TypeAccount, TypeFinance:
+		return true
+	}
+	return false
+}
