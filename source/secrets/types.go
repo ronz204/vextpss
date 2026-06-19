96ae5d1315ada5603f062992e22079d00a182b8e
roney ruiz <roneyruizrojas@gmail.com>
2026-06-18 22:02:56 -0600

    chore(secrets): format secret types comments

--- source/secrets/types.go
+++ source/secrets/types.go
@@ -5,8 +5,10 @@ const (
 	TypeFinance = "finance"
 )
 
+// ======================================
 // IsKnownType reports whether t is a registered secret type.
 // Add new constants here as new types are introduced.
+// ======================================
 func IsKnownType(t string) bool {
 	switch t {
 	case TypeAccount, TypeFinance:
