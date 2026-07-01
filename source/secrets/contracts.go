8c9a52c3d308d32fc5eb39650c248a98fc0f76c2
roney ruiz <roneyruizrojas@gmail.com>
2026-06-30 23:09:32 -0600

    feat(core): modeling secret encryptor complete args

--- source/secrets/contracts.go
+++ source/secrets/contracts.go
@@ -8,3 +8,8 @@ type Repository interface {
 	Update(ctx context.Context, secret Secret) error
 	ListAll(ctx context.Context) ([]Secret, error)
 }
+
+type Encryptor interface {
+	Encrypt(ctx context.Context, plaintext, password []byte) (salt, nonce, ciphertext []byte, err error)
+	Decrypt(ctx context.Context, salt, nonce, ciphertext []byte, password []byte) (plaintext []byte, err error)
+}
