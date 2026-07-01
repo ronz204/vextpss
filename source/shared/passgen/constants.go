3db329da6fc366336b77fd8d334f2f5b27a15902
roney ruiz <roneyruizrojas@gmail.com>
2026-06-30 23:34:36 -0600

    feat(infra): setup argon and aesgcm configurations

---
+++ source/shared/passgen/constants.go
@@ -0,0 +1,27 @@
+package passgen
+
+type Argon2Config struct {
+	Time    uint32
+	Memory  uint32
+	Threads uint8
+	KeyLen  uint32
+}
+
+type AESGCMConfig struct {
+	Argon    Argon2Config
+	SaltLen  int
+	NonceLen int
+}
+
+func DefaultConfig() AESGCMConfig {
+	return AESGCMConfig{
+		Argon: Argon2Config{
+			Time:    3,
+			Memory:  64 * 1024,
+			Threads: 2,
+			KeyLen:  32,
+		},
+		SaltLen:  16,
+		NonceLen: 12,
+	}
+}
